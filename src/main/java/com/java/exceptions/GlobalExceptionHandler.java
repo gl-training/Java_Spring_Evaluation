3fdaa85fa31d70ad66bd622657384325d0e52ae4
gl-training <michael.felipe@globallogic.com>
2026-01-16 14:48:14 -0500

    Remove unused code

--- src/main/java/com/java/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/java/exceptions/GlobalExceptionHandler.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
